5cc2b30eac002b9bfcfbf963c1afc4ab47c137b2
Arnove S <arnove16s@gmail.com>
2026-08-16 12:22:59 -0700

    Fix interaction timeout on vote command - defer response immediately before sync check

--- bot.py
+++ bot.py
@@ -23,7 +23,7 @@ intents.guilds = True
 bot = commands.Bot(command_prefix='/', intents=intents)
 
 # Bot version
-BOT_VERSION = "1.8.2"
+BOT_VERSION = "1.8.3"
 BOT_OWNER_ID = 807087691522375681  # Set this to your Discord ID for owner commands
 
 # Data storage files
